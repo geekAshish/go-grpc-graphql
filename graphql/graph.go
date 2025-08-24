ac83005927e3829b960109e12c8ba06b203fe284
Ashish Kushwaha <ashish@wishfy.club>
2025-08-24 21:05:32 +0530

    docker compose

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,32 +1,32 @@
 package graphql
 
 import (
-	"github.com/99designs/gqlgen/graphql"
+	"github.com/geekAshish/go-grpc-graphql-micro/account"
+	"github.com/geekAshish/go-grpc-graphql-micro/catalog"
 	"github.com/geekAshish/go-grpc-graphql-micro/graphql"
+	"github.com/geekAshish/go-grpc-graphql-micro/order"
 )
 
-
 type Server struct {
-	accountClient *account.Client 
-	catalogClient *catalog.Client 
-	orderClient *order.Client 
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
 }
 
-
 func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
-	accountClient, err := account.NewClient(accountUrl);
+	accountClient, err := account.NewClient(accountUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	catalogClient, err := catalog.NewClient(catalogUrl);
+	catalogClient, err := catalog.NewClient(catalogUrl)
 	if err != nil {
 		// catlog is depend on account client
 		accountClient.Close()
 		return nil, err
 	}
-	
-	orderClient, err := order.NewClient(orderUrl);
+
+	orderClient, err := order.NewClient(orderUrl)
 	if err != nil {
 		// order is depend on account, catalog client
 		accountClient.Close()
@@ -34,47 +34,34 @@ func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) 
 		return nil, err
 	}
 
-	return &Server {
+	return &Server{
 		accountClient,
 		catalogClient,
 		orderClient,
 	}, nil
 }
 
-
-
 func (s *Server) Mutation() MutationResolver {
-	return &mutationResolver {
+	return &mutationResolver{
 		server: s,
 	}
 }
-
 
 func (s *Server) Query() QueryResolver {
-	return &queryResolver {
+	return &queryResolver{
 		server: s,
 	}
 }
-
 
 func (s *Server) Account() AccountResolver {
-	return &accountResolver {
+	return &accountResolver{
 		server: s,
 	}
 }
 
-
 func (s *Server) ExecutableSchema() graphql.ExecutableSchema {
-	return NewExecutableSchema((
-		Config{
-			Resolvers: s,
-		}),
+	return NewExecutableSchema((Config{
+		Resolvers: s,
+	}),
 	)
 }
-
-
-
-
-
-
-
